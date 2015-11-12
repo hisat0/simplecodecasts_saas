aa09dcd7d753faffb1eb85b48be9cbade4b2d4b1
Juan Roz <ju4nr0z@gmail.com>
2015-11-12 03:43:58 +0000

    mail

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,9 +1,10 @@
 class ContactMailer < ActionMailer::Base
-    deafult to: 'ju4nr0z@gmail.com'
-    def contact_email(name, email, body)
-        @name = name
-        @email = email
-        @body = body
-        mail(from: @email, subject: 'Contact Form Message')
-    end
+  default to: 'ju4nr0z@gmail.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
+
